0551fd2fea3d0f1ec57d476f327ab42cd6dd51c8
nabil_yanogo <mediouni@yanogo.fr>
2022-12-01 09:22:10 +0100

    chore: Improve starter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './redux/store'
 
-test('renders learn react link', () => {
+test('renders react app screen', () => {
   render(
     <Provider store={store}>
       <App />
     </Provider>,
   )
-  const linkElement = screen.getByTestId('screen-app')
-  expect(linkElement).toBeInTheDocument()
+  const app = screen.getByTestId('screen-app')
+  expect(app).toBeInTheDocument()
 })
